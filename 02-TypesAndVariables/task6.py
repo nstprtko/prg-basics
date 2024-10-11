3aa3df41047ac64c1309087da7e8797b1fc2e1a4
Anastasiia Prytyko <0never.mind2610@gmail.com>
2024-10-11 12:40:51 +0200

    task 6 completed

---
+++ 02-TypesAndVariables/task6.py
@@ -0,0 +1,9 @@
+import math
+height_person = 1.65
+height_hotel = 20
+
+distance_person = 3.57 * math.sqrt(height_person) 
+distance_hotel = 3.57 * math.sqrt(height_hotel) 
+
+print(distance_person)
+print(distance_hotel)
